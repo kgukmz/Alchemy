2bf2692b985e687dd13e43fba1d6725c3d7f9ed6
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-28 18:41:35 +0000

    new stuff

---
+++ Files/Utils/loadEnv.lua
@@ -0,0 +1,35 @@
+local OldRequire = require
+
+local RequireCache = {}
+local ServiceCache = {}
+
+local function GetService(Service)
+    if (ServiceCache[Service] ~= nil) then
+        return ServiceCache[Service]
+    end
+
+    local NewService = game:GetService(Service)
+    ServiceCache[Service] = NewService
+
+    return NewService
+end
+
+local function RequireHook(Value)
+    if (not (typeof(Value) == "string")) then
+        local Module = OldRequire(Value)
+        return Module
+    end
+
+    if (RequireCache[Value] ~= nil) then
+        local CachedDirectory = RequireCache[Value]
+        return CachedDirectory()
+    end
+
+    local RequiredDirecory = loadstring(game:HttpGet("https://github.com/kgukmz/Alchemy/raw/refs/heads/main/" .. Value))
+    RequireCache[Value] = RequiredDirecory
+
+    return RequiredDirecory()
+end
+
+getgenv().require = RequireHook
+getgenv().GetService = GetService
--- loader.lua
+++ loader.lua
@@ -1,3 +1,10 @@
 local Success, Error = pcall(function()
-    loadstring(game:HttpGet(""))
+    loadstring(game:HttpGet("https://github.com/kgukmz/Alchemy/raw/refs/heads/main/Files/Utils/loadEnv.lua"))()
 end)
+
+if (Success == false) then
+    warn("Unable to load script environment")
+    return
+end
+
+require("Files/Utils/ESPModule.lua")
