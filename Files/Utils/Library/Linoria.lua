1fe5825b2ebeb288e128714059aae008b2d29b8d
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-29 22:20:42 +0000

    Tet

--- Files/Utils/Library/Linoria.lua
+++ Files/Utils/Library/Linoria.lua
@@ -3003,10 +3003,10 @@ function Library:CreateWindow(...)
     });
 
     local WindowLabel = Library:CreateLabel({
-        --Position = UDim2.new(0, 7, 0, 0);
+        Position = UDim2.new(0, 7, 0, 0);
         Size = UDim2.new(0, 0, 0, 25);
         Text = Config.Title or '';
-        TextXAlignment = Enum.TextXAlignment.Left;
+        TextXAlignment = Config.CenteredTitle == true and Enum.TextXAlignment.Center or Enum.TextXAlignment.Left;
         ZIndex = 1;
         Parent = Inner;
     });
