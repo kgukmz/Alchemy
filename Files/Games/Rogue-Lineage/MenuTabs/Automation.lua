7e1da831c1bb5de74b745b74402c051780e9489a
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-31 20:48:13 +0000

    haha stffs

--- Files/Games/Rogue-Lineage/MenuTabs/Automation.lua
+++ Files/Games/Rogue-Lineage/MenuTabs/Automation.lua
@@ -1,6 +1,13 @@
 local Automation = {}
 local TabGroups = {}
 
+function TabGroups:Test(WindowTab)
+    local testtabbox = WindowTab:AddTabbox({ Side = 1; })
+
+    testtabbox:AddTab("[ TEST 1 ]")
+    testtabbox:AddTab("[ TEST 2 ]")
+end
+
 function Automation:Init(Window)
     local AutomationTab = Window:AddTab("[ AUTOMATION ]")
 end
