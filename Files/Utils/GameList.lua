5ab11746d874d3865d956fcdfda42057d1c31145
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-02-08 20:20:43 +0000

    Bye,

--- Files/Utils/GameList.lua
+++ Files/Utils/GameList.lua
@@ -2,5 +2,5 @@ return {
     -- // a literadl baseplate id lol [4483381587] = "Rogue-Lineage";
     -- // [5208655184] = "Rogue-Lineage";
     -- // [4483381587] = "Deepwoken";
-    [4483381587] = "Rogue-Lineage";
+    [5208655184] = "Rogue-Lineage";
 }
